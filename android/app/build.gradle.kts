50189533a89fd9d5784c54e150a744454a042196
xqd <xqd922@gmail.com>
2026-06-02 23:57:24 +0800

    Harden Android package visibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -98,7 +98,4 @@ dependencies {
     implementation(project(":common"))
     implementation(libs.core.splashscreen)
     implementation(libs.gson)
-    implementation(libs.smali.dexlib2) {
-        exclude(group = "com.google.guava", module = "guava")
-    }
 }
