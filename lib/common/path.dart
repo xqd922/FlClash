89d354586e661948d3ed22eadd8b94959fc5090b
seven <xqdwmq@gmail.com>
2026-06-16 16:03:55 +0800

    refactor: complete app name unification to XClash
    - Update distribute_options.yaml app_name
    - Update core binary path and lock file name in path.dart
    - Update Linux packaging configs (AppImage, DEB, RPM) display names and keywords

--- lib/common/path.dart
+++ lib/common/path.dart
@@ -44,7 +44,7 @@ class AppPath {
   }
 
   String get corePath {
-    return join(executableDirPath, 'FlClashCore$executableExtension');
+    return join(executableDirPath, 'XClashCore$executableExtension');
   }
 
   String get helperPath {
@@ -83,7 +83,7 @@ class AppPath {
 
   Future<String> get lockFilePath async {
     final homeDirPath = await appPath.homeDirPath;
-    return join(homeDirPath, 'FlClash.lock');
+    return join(homeDirPath, 'XClash.lock');
   }
 
   Future<String> get configFilePath async {
